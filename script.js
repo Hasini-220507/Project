10682e0139f886bea81a3ab6f7f672e680aa4b5c
Hasini-220507 <n220507@rguktn.ac.in>
2026-01-06 05:48:50 -0500

    added succesfully

---
+++ script.js
@@ -0,0 +1,14 @@
+  function showoffer(){
+            alert("🎉 Flat 35% off")
+        }
+
+        function placeorder(){
+let result=confirm("Are u sure to add in to the cart")
+if(result){
+    alert("Added to cart successfully ✅")
+}
+else{
+    alert("order cancelled ❌")
+}
+
+}
